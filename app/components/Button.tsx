e2ade3e1ce8f877edb2073eb81757c0c3f563bef
sarim-alis <sarimslayerali786@gmail.com>
2025-12-01 06:52:17 +0500

    Button setup

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { IconType } from "react-icons";
 
 interface ButtonProps {
@@ -15,27 +17,39 @@ const Button: React.FC<ButtonProps> = ({
     disabled,
     outline,
     small,
-    icon
+    icon: Icon
 }) => {
     return (
       <button
+        onClick={onClick}
+        disabled={disabled}
         className={`
-            relatove
+            relative
             disabled:opacity-70
             disabled:cursor-not-allowed
             rounded-lg
-            hover-opacity-80
+            hover:opacity-80
             transition
             w-full
-            ${outline ? 'bg-white': 'bg-rose-500'},
-            ${outline ? 'border-black': 'border-rose-500'},
-            ${outline ? 'text-black': 'text-white'},
-            ${outline ? 'py-1': 'py-3'},
-            ${outline ? 'text-sm': 'text-md'},
-            ${outline ? 'font-light': 'font-semibold'},
-            ${outline ? 'border': 'border-2'},
-        `}  
+            ${outline ? 'bg-white' : 'bg-rose-500'}
+            ${outline ? 'border-black' : 'border-rose-500'}
+            ${outline ? 'text-black' : 'text-white'}
+            ${small ? 'py-1' : 'py-3'}
+            ${small ? 'text-sm' : 'text-md'}
+            ${small ? 'font-light' : 'font-semibold'}
+            ${outline ? 'border' : 'border-2'}
+        `}
       >
+        {Icon && (
+            <Icon
+             size={24}
+             className="
+              absolute
+              left-4
+              top-3
+             "
+            />
+        )}
         {label}
       </button>
     );
