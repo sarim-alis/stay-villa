6b4bea66b86c41f412ae0a177339c53834383683
sarim-alis <sarimslayerali786@gmail.com>
2025-12-02 14:14:42 +0500

    feat/use register modal

--- app/hooks/useRegisterModal.ts
+++ app/hooks/useRegisterModal.ts
@@ -11,3 +11,5 @@ const useRegisterModal = create<RegisterModalStore>((set) => ({
     onOpen:  () => set ({ isOpen: true }),
     onClose: () => set ({ isOpen: false}),
 }));
+
+export default useRegisterModal;
