612143b8801043a081a26a03464d2a373d73b84d
sarim-alis <sarimslayerali786@gmail.com>
2025-12-02 14:33:03 +0500

    feat/Register model setup

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,4 +1,16 @@
-import React from 'react';
+'use client';
+
+import axios from 'axios';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { useCallback, useState } from 'react';
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm
+} from 'react-hook-form';
+
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const RegisterModal: React.FC = () => {
   return (
