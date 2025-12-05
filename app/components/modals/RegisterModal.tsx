97eade5e51d027a8a4c1735673dba52d26361a26
sarim-alis <sarimslayerali786@gmail.com>
2025-12-05 07:07:01 +0500

    feats/prisma db sets

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -15,6 +15,7 @@ import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
+import Button from '../Button';
 
 const RegisterModal: React.FC = () => {
     const registerModal = useRegisterModal();
@@ -81,6 +82,49 @@ const RegisterModal: React.FC = () => {
               required 
             />
         </div>
+    );
+
+    const footerContent = (
+      <div className='flex flex-col gap-4 mt-3'>
+        <hr />
+        <Button 
+          outline
+          label="Continue with Google"
+          icon={FcGoogle}
+          onClick={() => {}}
+        />
+        <Button 
+          outline
+          label="Continue with Github"
+          icon={AiFillGithub}
+          onClick={() => {}}
+        />
+        <div
+          className="
+          text-neutral-500
+          text-center
+          mt-4
+          font-light
+          "
+        >
+          <div className='
+          justify-center text-center flex flex-row items-center gap-2'>
+            <div>
+              Already have an account?
+            </div>
+            <div
+              onClick={registerModal.onClose}
+              className='
+               text-neutral-800
+               cursor-pointer
+               hover:underline
+               '
+            >
+              Log in
+            </div>
+          </div>
+        </div>
+      </div>
     )
 
   return (
@@ -92,6 +136,7 @@ const RegisterModal: React.FC = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
