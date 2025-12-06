56eeb65d627f53dc208f4f6f6a479937974c6b3d
sarim-alis <sarimslayerali786@gmail.com>
2025-12-06 06:46:47 +0500

    feat/getCurrentUser

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,24 +1,30 @@
 'use client';
 
+// Imports.
 import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
-import {
-    FieldValues,
-    SubmitHandler,
-    useForm
-} from 'react-hook-form';
-
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
 import Button from '../Button';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
+
+
+
+
+// Frontend.
 const LoginModal: React.FC = () => {
-    const registerModal = useLoginModal();
+    const router = useRouter()
+    const registerModal = useRegisterModal();
+    const loginModal    = useLoginModal();
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -29,7 +35,6 @@ const LoginModal: React.FC = () => {
         }
     } = useForm<FieldValues>({
         defaultValues: {
-            name: '',
             email: '',
             password: ''
         }
@@ -38,35 +43,34 @@ const LoginModal: React.FC = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
         setIsLoading(true);
 
-        axios.post('/api/register', data)
-          .then(() => {
-            registerModal.onClose();
-          })
-          .catch((error) => {
-            toast.error('Something went wrong');
-          })
-          .finally(() => {
-            setIsLoading(false);
-          })
+        signIn('credentials', {
+          ...data,
+          redirect: false,
+        })
+        .then((callback) => {
+          setIsLoading(false);
+
+          if (callback?.ok) {
+            toast.success('Logged in');
+            router.refresh();
+            loginModal.onClose();
+          }
+
+          if (callback?.error) {
+            toast.error(callback.error);
+          }
+        })
     }
 
     const bodyContent = (
         <div className='flex flex-col gap-4'>
             <Heading 
-                title="Welcome to Airbnb"
-                subtitle="Create an account!"
+                title="Welcome back"
+                subtitle="Login to your account!"
             />
             <Input
               id="email"
               label="Email"
-              disabled={isLoading}
-              register={register}
-              errors={errors}
-              required 
-            />
-            <Input
-              id="name"
-              label="Name"
               disabled={isLoading}
               register={register}
               errors={errors}
@@ -130,10 +134,10 @@ const LoginModal: React.FC = () => {
   return (
     <Modal
       disabled={isLoading}
-      isOpen={registerModal.isOpen}
-      title="Register"
+      isOpen={loginModal.isOpen}
+      title="Login"
       actionLabel="Continue"
-      onClose={registerModal.onClose}
+      onClose={loginModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
       footer={footerContent}
