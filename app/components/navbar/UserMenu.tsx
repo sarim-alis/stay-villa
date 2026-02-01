4e91cd17d0e5adac5be06ba126581841ac26bdc3
sarim-alis <sarimslayerali786@gmail.com>
2026-02-02 00:20:30 +0500

    feat reservations done

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
               label="My trips"
             />
             <MenuItem
-              onClick={() => {}}
+               onClick={() => router.push('/favorites')}
               label="My favorites"
             />
             <MenuItem
