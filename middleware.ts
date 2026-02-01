e3f24400d8efae93a87c5782d654550306d897a0
sarim-alis <sarimslayerali786@gmail.com>
2026-02-02 04:51:02 +0500

    feat middleware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware(req) {
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token
+    }
+  }
+);
+
+export const config = {
+  matcher: [
+    '/trips',
+    '/reservations',
+    '/properties',
+    '/favorites',
+  ]
+};
