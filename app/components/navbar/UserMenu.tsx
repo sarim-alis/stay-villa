24643c16d3fce3a4f86ec3ce385d09e5dbe0b3f7
sarim-alis <sarimslayerali786@gmail.com>
2026-02-01 21:21:26 +0500

    feat: cancel reservation

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
 import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 
 
@@ -19,6 +20,7 @@ interface UserMenuProps {
 const UserMenu: React.FC<UserMenuProps> = ({
   currentUser
 }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal    = useLoginModal();
   const rentModal     = useRentModal();
@@ -101,7 +103,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             {currentUser ? (
             <>
             <MenuItem
-              onClick={() => {}}
+              onClick={() => router.push('/trips')}
               label="My trips"
             />
             <MenuItem
--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import EmptyState from "../components/EmptyState";
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+import TripsClient from "./TripsClient";
 
-const Trips: React.FC = () => {
-  return (
-    <div>Trips</div>
-  );
-};
 
-export default Trips;
+const TripsPage = async () => {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+     return (
+      <div>
+        <EmptyState
+            title="Unauthorized"
+            subtitle="Please login"
+        />
+      </div>
+     )
+   }
+
+   const reservations = await getReservations({
+        userId: currentUser.id
+   });
+
+   if (reservations.length === 0) {
+    return (
+      <div>
+        <EmptyState
+            title="No trips found"
+            subtitle="Looks like you have not reserved any trips."
+        />
+      </div>
+    )
+   }
+
+   return (
+    <div>
+      <TripsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </div>
+   )
+}
+
+export default TripsPage;
